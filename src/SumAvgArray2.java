73ff9c5aa2368743d87664e930b1511a7b9be940
CaiqueBottosso <caiquecoleto@gmail.com>
2023-04-17 18:49:28 +0100

    SumArray2

---
+++ src/SumAvgArray2.java
@@ -0,0 +1,10 @@
+public class SumAvgArray2 {
+    public static void main(String[] args) {
+        int[] number = {1,2,3,4,5,6,7,8,9,10};
+        //sum = number[0]+number[1]+number[2]+number[3]+number[4]+number[5]+number[6]+number[7]+number[8]+number[9];
+        for (int sum = 1; sum <11; sum = sum + 1){
+            System.out.println("the value in the array is : " + sum );
+        }
+        //System.out.println("The sum of the array is: " + sum);
+    }
+}
