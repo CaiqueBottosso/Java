f228d8ced449b7f1e918a74d7a20c72fafcb50ac
CaiqueBottosso <caiquecoleto@gmail.com>
2023-06-19 18:46:55 +0100

    update

---
+++ src/JavaIO.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.IOException;
+
+public class JavaIO {
+    public static void main(String[] args) {
+        // to handle errors, we use try-catch block
+        try {
+            File obj = new File("Courses to make .txt");
+            if (obj.createNewFile()){
+                System.out.println("File created");
+            } else {
+                System.out.println("File already exist.");
+            }
+        }catch (IOException e){
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+
+}
